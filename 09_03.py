c0816194f8f052fc00d3713743f7383979a1c9c8
Dishant Raut <dishantraut@gmail.com>
2021-02-15 18:21:27 +0530

    Pattern 3 (Python)

---
+++ 09_03.py
@@ -0,0 +1,20 @@
+for i in range(1,7):
+    for k in range(1,i):
+        print("*",end=" ")
+    print("")
+
+for i in range(1,7):
+    for k in range(6,i,-1):
+        print("*",end=" ")
+    print("")
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
