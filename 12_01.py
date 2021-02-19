738a7488bbafc75c0e281441582f6c008d9ebab6
Dishant Raut <dishantraut@gmail.com>
2021-02-20 02:25:11 +0530

    String Operations (Python)

---
+++ 12_01.py
@@ -0,0 +1,26 @@
+""" String Operations """
+
+a = "My String is Dishant Raut"
+
+print(a.lower())
+print(a.upper())
+print(a.title())
+print(a.capitalize())
+print(a.swapcase())
+print(a.replace("String","Name"))
+print(a.count("a"))
+print(a.find("Dishant"))
+print(a[a.find("is") : ])
+print(a.split("is"))
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+print(a.isalpha())
+print(a.isdigit())
+print(a.startswith("M"))
+print(a.endswith("t"))
+
+a = "Dishant"
+b = "Raut"
+print(f"My name is {a} {b}")
+
