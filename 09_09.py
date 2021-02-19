4958d8ad931a103975ca4127a017d35c48141888
Dishant Raut <dishantraut@gmail.com>
2021-02-20 00:30:13 +0530

    Pattern 9 (Python)

---
+++ 09_09.py
@@ -0,0 +1,20 @@
+for i in range(1,6):
+    for  k in range(1,i):
+        print(" ",end="")
+    print("*")
+
+for i in range(1,6):
+    for  k in range(5,i,-1):
+        print(" ",end="")
+    print("*")
+# *
+#  *
+#   *
+#    *
+#     *
+#     *
+#    *
+#   *
+#  *
+# *
+
