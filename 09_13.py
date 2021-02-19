e8ec073010651aa1451ab4aee1276ea753ec3f58
Dishant Raut <dishantraut@gmail.com>
2021-02-20 00:58:57 +0530

    Pattern 13 (Python)

---
+++ 09_13.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+    for k in range(1,6):
+        if (i > 1) and (k > 1) and (i < 5) and (k < 5):
+            print(" ",end="")
+        else:
+            print("*",end="")
+    print("")
+
